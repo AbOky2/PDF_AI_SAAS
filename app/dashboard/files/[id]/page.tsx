4c795470806ed5abeb3985c839a8d0fc4ae1c471
AbOky2 <issaokiabderamane@gmail.com>
2024-12-14 19:38:44 +0100

    adding react pdf

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,6 +1,7 @@
+import PdfView from "@/components/PdfView";
 import { adminDb } from "@/firebaseAdmin";
 import { auth } from "@clerk/nextjs/server";
-function ChatToFilePage({
+async function ChatToFilePage({
     params : {id},
 }: {
     params : {id : string; }
@@ -17,9 +18,21 @@ function ChatToFilePage({
      .doc(id)
      .get();
 
+    const url = ref.data()?.downloadUrl;
+
   return (
-    <div>
-      Chat to file page {id}
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/*Right*/}
+
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        {/* Chat */}
+      </div>
+
+      {/* Left */}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+        {/*PDFView*/}
+        <PdfView url={url}/>
+      </div>
     </div>
   )
 }
---
+++ components/PdfView.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PdfView({url} : {url : string}) {
+  return (
+    <div>
+      Pdf view 
+    </div>
+  )
+}
+
+export default PdfView
+ 
