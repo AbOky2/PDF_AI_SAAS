bacc8a6d8c61869cb2d91acae883b99478bd131f
AbOky2 <issaokiabderamane@gmail.com>
2024-12-23 02:42:15 +0100

    wip3

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import Chat from "@/components/Chat";
 import PdfView from "@/components/PdfView";
 import { adminDb } from "@/firebaseAdmin";
 import { auth } from "@clerk/nextjs/server";
 
+//@ts-ignore
 async function ChatToFilePage({ params }: { params: { id: string } }) {
   const { id } = params; // On récupère id après résolution de params
 
