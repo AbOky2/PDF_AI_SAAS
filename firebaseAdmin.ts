f186825168c400b7927876dee360bc426a26cc82
AbOky2 <issaokiabderamane@gmail.com>
2024-12-23 02:26:16 +0100

    fixing service import probleme

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,17 +1,15 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 
 import { initializeApp,getApp, App, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-//@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const service_key = require('@/service_key.json');
+import { ServiceAccount } from "firebase-admin";
+import service_key from '@/service_key.json';
 
 let app : App;
 if (getApps().length===0){
     app = initializeApp({
-        credential : cert(service_key),
+        credential : cert(service_key as ServiceAccount),
     });
 }
 else{
